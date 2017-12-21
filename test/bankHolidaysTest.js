47e3e8be339b83f5ebb148c8148d1492d5f9c3ab
Robert Curran <ro5635@gmail.com>
2017-12-21 11:03:28 +0000

    Setup Mocha test environment
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .gitignore
    	new file:   test/bankHolidaysTest.js

---
+++ test/bankHolidaysTest.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+describe('Bank Holiday Dates Test', function() {
+    describe('Easter Bank Holiday', function() {
+        it('2017 Date Check', function() {
+            assert.equal(1, 0);
+        });
+
+        it('2018 Date Check', function() {
+            assert.equal(1, 0);
+        });
+
+        it('2019 Date Check', function() {
+            assert.equal(1, 0);
+        });
+
+        it('2020 Date Check', function() {
+            assert.equal(1, 0);
+        });
+
+        it('2030 Date Check', function() {
+            assert.equal(1, 0);
+        });
+
+        it('2035 Date Check', function() {
+            assert.equal(1, 0);
+        });
+
+        it('1990 Date Check', function() {
+            assert.equal(1, 0);
+        });
+    });
+});
